f183a41990f1f448edc81689f2a5d45159c00e57
Mathieu Baudier <mbaudier@argeo.org>
2025-06-24 12:06:36 +0200

    Remove common dev package

--- debian/cmake/debian-whisper.cpp.cmake
+++
@@ -1,4 +0,0 @@
-# install common
-install(DIRECTORY ${CMAKE_BINARY_DIR}/examples/ DESTINATION lib/${CMAKE_LIBRARY_ARCHITECTURE}/whisper.cpp/common FILES_MATCHING PATTERN "libcommon*.a" )
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/examples/ DESTINATION include/whisper.cpp/common FILES_MATCHING PATTERN "common*.h" )
-
